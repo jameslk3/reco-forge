a97d3e9597ebfa67c44547e6831a8aeb694a93f7
jkim826illinois <jkim826@illinois.edu>
2024-03-21 16:39:13 -0500

    added some comments for extract_data

--- src/lib.rs
+++ src/lib.rs
@@ -20,9 +20,20 @@ impl fmt::Display for Data {
     }
 }
 
+/// Receives the path of a JSON file as a &String. The function tries to open
+/// the file. If it doesn't, it will return Err.
+/// After it opens the file, it deserializes the JSON file into a vector of Data objects.
+/// If it successfully does so, it will return Ok. If it doesn't, it will return Err.
+/// 
+/// @param `file_name` - a String containing the file path of the JSON file
+/// 
+/// @return `Ok()` with the vector of data encapsulated in a `Result` enum [OR] `Err()` if the 
+/// file didn't open or didn't deserialize
 pub fn extract_data(file_name: &String) -> Result<Vec<Data>, ()> {
+    // Opens file
     let file = File::open(file_name).expect("File didn't open");
 
+    // deserializes into Data object
     let vector_of_data: Vec<Data> = serde_json::from_reader(file).expect("Deserialization failed");
 
     return Ok(vector_of_data);
