27893ca18073bd4a0a9a52b6321106f2e9c199ff
jkim826illinois <jkim826@illinois.edu>
2024-03-20 21:19:21 -0500

    extract_data ready for testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,20 @@
+extern crate serde;
+extern crate serde_json;
+
+use serde::Deserialize;
 use std::{fs::File, io::Read};
 
-use json;
 
-struct Data {
+#[derive(Debug, Deserialize, Clone)]
+pub struct Data {
     name: String,
     description: String,
     tags: Vec<String>
 }
 
-pub fn extract_data(file_name: &String) -> Result<(Data), ()> {
+pub fn extract_data(file_name: &String) -> Result<Vec<Data>, ()> {
     let file_result = File::open(file_name);
+    let mut vector_of_data: Vec<Data> = Vec::new();
     if file_result.is_err() {
         return Err(());
     }
@@ -19,6 +24,19 @@ pub fn extract_data(file_name: &String) -> Result<(Data), ()> {
     if read.is_err() {
         return Err(());
     }
-    let parsed = json::parse(&contents);
-    return Err(());
+    let contents_split: Vec<&str> = contents.split(",").collect();
+    let mut contents_split_string: Vec<String> = Vec::new();
+    for x in 0..contents_split.len() {
+        contents_split_string.push(contents_split[x].to_string());
+    }
+
+    for x in 0..contents_split.len() {
+        let deserialize = serde_json::from_str::<Data>(&contents_split_string[x]);
+        if deserialize.is_err() {
+            return Err(());
+        }
+        vector_of_data.push(deserialize.unwrap().clone());
+    }
+    
+    return Ok(vector_of_data);
 }
