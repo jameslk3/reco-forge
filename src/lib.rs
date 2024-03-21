49b6608f8057a50c2197ae44e9794eb9d3c00b49
jkim826illinois <jkim826@illinois.edu>
2024-03-20 19:51:51 -0500

    starting extract_data fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+use std::{fs::File, io::Read};
+
+use json;
+
 struct Data {
-    
+    name: String,
+    description: String,
+    tags: Vec<String>
+}
+
+pub fn extract_data(file_name: &String) -> Result<(Data), ()> {
+    let file_result = File::open(file_name);
+    if file_result.is_err() {
+        return Err(());
+    }
+    let mut file = file_result.unwrap();
+    let mut contents = String::new();
+    let read = file.read_to_string(&mut contents);
+    if read.is_err() {
+        return Err(());
+    }
+    let parsed = json::parse(&contents);
+    return Err(());
 }
