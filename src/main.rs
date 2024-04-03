5ed7e90181b7b98b6505b6355337fc3349435f32
jkim826illinois <jkim826@illinois.edu>
2024-04-02 21:51:18 -0500

    pushing small changes

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,17 @@ use reco_forge::functions::*;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let start = std::time::Instant::now();
     let mut nodes: Vec<DataWithEmbeddings> = extract_data(&String::from("./src/sample-json/games_clean_mini.json")).unwrap();
-    let result = get_embeddings(&mut nodes);
+    let _result = get_embeddings(&mut nodes);
     let end = start.elapsed();
     println!("Time elapsed: {:?}", end);
+
+	for x in 0..nodes.len() {
+		if nodes[x].embedding.is_some() {
+			println!("THERE IS SOMETHING IN THE EMBEDDING {}", x);
+		} else {
+			println!("NOPE {}", x);
+		}
+	}
 
     Ok(())
 }
